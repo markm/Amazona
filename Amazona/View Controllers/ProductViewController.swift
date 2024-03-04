bdcf4907ecfcd8a4bf93ddcb9cd260ddb274cc5d
Mark Mckelvie <markmck1@gmail.com>
2024-03-04 03:48:57 -0500

    Create ProductViewController.swift

---
+++ Amazona/View Controllers/ProductViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProductViewController.swift
+//  Amazona
+//
+//  Created by Mark Mckelvie on 3/4/24.
+//
+
+import Foundation
+import UIKit
+
+class ProductViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
