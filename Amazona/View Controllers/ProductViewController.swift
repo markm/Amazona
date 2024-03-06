2d148c04bf43fe3ae56a2a93c0e40410b46b2023
Mark Mckelvie <markmck1@gmail.com>
2024-03-06 01:29:06 -0500

    Refactors Product View Controller

--- Amazona/View Controllers/ProductViewController.swift
+++ Amazona/View Controllers/ProductViewController.swift
@@ -12,121 +12,6 @@ class ProductViewController: UIViewController {
     
     var product: Product
     
-    init(product: Product) {
-        self.product = product
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        setupViews()
-        configureViews()
-        configureProduct()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
-    }
-    
-    private func configureProduct() {
-        titleLabel.text = product.title
-        priceLabel.text = "\(product.price.formatted(.currency(code: "EUR")))"
-        descriptionLabel.text = product.descriptionText
-        ratingLabel.text = "\(product.rating?.rate ?? 0)"
-        ratingCountLabel.text = "(\(product.rating?.count ?? 0) ratings)"
-        categoryLabel.text = product.category
-        /// Load image from URL
-        if let imageURL = URL(string: product.imageURLString) {
-            URLSession.shared.dataTask(with: imageURL) { (data, _, error) in
-                if let data = data {
-                    DispatchQueue.main.async {
-                        self.productImageView.image = UIImage(data: data)
-                    }
-                }
-            }.resume()
-        }
-    }
-    
-    private func setupViews() {
-        view.addSubview(productImageView)
-        view.addSubview(titleLabel)
-        view.addSubview(descriptionLabel)
-        view.addSubview(ratingStackView)
-        view.addSubview(buyNowButton)
-        
-        bottomStackView.addArrangedSubview(categoryContainerView)
-        bottomStackView.addArrangedSubview(priceLabel)
-        ratingStackView.addArrangedSubview(ratingStarImageView)
-        ratingStackView.addArrangedSubview(ratingLabel)
-        ratingStackView.addArrangedSubview(ratingCountLabel)
-        
-        view.addSubview(bottomStackView)
-        categoryContainerView.addSubview(categoryLabel)
-        
-        buyNowButton.addTarget(self, action: #selector(buyNowWasSelected), for: .touchUpInside)
-    }
-    
-    private func configureViews() {
-        productImageView.easy.layout(
-            Top(kMediumPadding).to(view.safeAreaLayoutGuide, .top),
-            CenterX(),
-            Height(*0.45).like(view.safeAreaLayoutGuide, .height),
-            Leading(kMediumPadding).to(view.safeAreaLayoutGuide, .leading),
-            Trailing(kMediumPadding).to(view.safeAreaLayoutGuide, .trailing)
-        )
-        
-        titleLabel.easy.layout(
-            Top(kMediumPadding).to(productImageView, .bottom),
-            CenterX(),
-            Leading(kMediumPadding).to(view.safeAreaLayoutGuide, .leading),
-            Trailing(kMediumPadding).to(view.safeAreaLayoutGuide, .trailing)
-        )
-        
-        descriptionLabel.easy.layout(
-            Top(kSmallPadding).to(titleLabel, .bottom),
-            CenterX(),
-            Leading(kMediumPadding).to(view.safeAreaLayoutGuide, .leading),
-            Trailing(kMediumPadding).to(view.safeAreaLayoutGuide, .trailing)
-        )
-        
-        ratingStackView.easy.layout(
-            Top(kSmallPadding).to(descriptionLabel, .bottom),
-            Leading(kMediumPadding)
-        )
-        
-        bottomStackView.easy.layout(
-            Leading(kSmallPadding),
-            Trailing(kSmallPadding),
-            Top(kMediumPadding).to(ratingStackView, .bottom)
-        )
-        
-        categoryContainerView.easy.layout(
-            Height().like(bottomStackView),
-            Width(*0.5).like(bottomStackView)
-        )
-        
-        categoryLabel.easy.layout(
-            Center()
-        )
-        
-        priceLabel.easy.layout(
-            Height().like(bottomStackView)
-        )
-        
-        buyNowButton.easy.layout(
-            Top(kMediumPadding).to(bottomStackView, .bottom),
-            Leading(kSmallPadding),
-            Trailing(kSmallPadding),
-            Height(50)
-        )
-    }
-    
     private let productImageView: UIImageView = {
         let productImageView = UIImageView()
         productImageView.contentMode = .scaleAspectFit
@@ -228,20 +113,145 @@ class ProductViewController: UIViewController {
         button.titleLabel?.font = AppFonts.helveticaNeue(ofSize: 18, weight: .bold)
         button.backgroundColor = UIColor.AmazonaVibrantGreen
         button.layer.cornerRadius = 25
-        
         /// apply shadow
         button.layer.shadowColor = UIColor.darkGray.cgColor
         button.layer.shadowOffset = CGSize(width: 0, height: 5)
         button.layer.shadowRadius = 5
         button.layer.shadowOpacity = 0.3
         button.layer.masksToBounds = false
-        
         /// enable autolayout - using easy peasy library
         button.translatesAutoresizingMaskIntoConstraints = false
-
         return button
     }()
+    
+    // MARK: - Initializers
+    
+    init(product: Product) {
+        self.product = product
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+        layoutViews()
+        configureProduct()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    // MARK: - Setup Methods for UI Elements
+    
+    private func setupViews() {
+        view.addSubview(productImageView)
+        view.addSubview(titleLabel)
+        view.addSubview(descriptionLabel)
+        view.addSubview(ratingStackView)
+        view.addSubview(buyNowButton)
+        
+        bottomStackView.addArrangedSubview(categoryContainerView)
+        bottomStackView.addArrangedSubview(priceLabel)
+        ratingStackView.addArrangedSubview(ratingStarImageView)
+        ratingStackView.addArrangedSubview(ratingLabel)
+        ratingStackView.addArrangedSubview(ratingCountLabel)
+        
+        view.addSubview(bottomStackView)
+        categoryContainerView.addSubview(categoryLabel)
+        
+        buyNowButton.addTarget(self, action: #selector(buyNowWasSelected), for: .touchUpInside)
+    }
+    
+    // MARK: - Layout Methods for UI Elements
+    
+    private func layoutViews() {
+        productImageView.easy.layout(
+            Top(kMediumPadding).to(view.safeAreaLayoutGuide, .top),
+            CenterX(),
+            Height(*0.45).like(view.safeAreaLayoutGuide, .height),
+            Leading(kMediumPadding).to(view.safeAreaLayoutGuide, .leading),
+            Trailing(kMediumPadding).to(view.safeAreaLayoutGuide, .trailing)
+        )
+        
+        titleLabel.easy.layout(
+            Top(kMediumPadding).to(productImageView, .bottom),
+            CenterX(),
+            Leading(kMediumPadding).to(view.safeAreaLayoutGuide, .leading),
+            Trailing(kMediumPadding).to(view.safeAreaLayoutGuide, .trailing)
+        )
+        
+        descriptionLabel.easy.layout(
+            Top(kSmallPadding).to(titleLabel, .bottom),
+            CenterX(),
+            Leading(kMediumPadding).to(view.safeAreaLayoutGuide, .leading),
+            Trailing(kMediumPadding).to(view.safeAreaLayoutGuide, .trailing)
+        )
+        
+        ratingStackView.easy.layout(
+            Top(kSmallPadding).to(descriptionLabel, .bottom),
+            Leading(kMediumPadding)
+        )
+        
+        bottomStackView.easy.layout(
+            Leading(kSmallPadding),
+            Trailing(kSmallPadding),
+            Top(kMediumPadding).to(ratingStackView, .bottom)
+        )
+        
+        categoryContainerView.easy.layout(
+            Height().like(bottomStackView),
+            Width(*0.5).like(bottomStackView)
+        )
+        
+        categoryLabel.easy.layout(
+            Center()
+        )
+        
+        priceLabel.easy.layout(
+            Height().like(bottomStackView)
+        )
+        
+        buyNowButton.easy.layout(
+            Top(kMediumPadding).to(bottomStackView, .bottom),
+            Leading(kSmallPadding),
+            Trailing(kSmallPadding),
+            Height(50)
+        )
+    }
+    
+    // MARK: Configuration Methods for UI Elements
+    
+    @MainActor
+    private func configureProduct() {
+        titleLabel.text = product.title
+        priceLabel.text = "\(product.price.formatted(.currency(code: "EUR")))"
+        descriptionLabel.text = product.descriptionText
+        ratingLabel.text = "\(product.rating?.rate ?? 0)"
+        ratingCountLabel.text = "(\(product.rating?.count ?? 0) ratings)"
+        categoryLabel.text = product.category
+        
+        /// Load image from URL
+        if let imageURL = URL(string: product.imageURLString) {
+            URLSession.shared.dataTask(with: imageURL) { (data, _, error) in
+                if let data = data {
+                    DispatchQueue.main.async {
+                        self.productImageView.image = UIImage(data: data)
+                    }
+                }
+            }.resume()
+        }
+    }
 
+    // MARK: - Actions
 
     @objc private func buyNowWasSelected() {
         print("Buy Now was selected")
